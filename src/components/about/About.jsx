6ac52738dffd5ebf68774d9d709dd089c9448e14
Nick Remley <remleynick@yahoo.com>
2022-12-31 15:33:09 -0700

    About me and nav

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './about.css'
+import Programmer from '../../assests/programmer-two.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className="about_me">
+          <div className='about_me-image'>
+            <img src={Programmer} alt='Image of Programmer' />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className='about_card'>
+              <h5>Passionate</h5>
+            </article>
+            <article className='about_card'>
+              <h5>Driven</h5>
+            </article>
+            <article className='about_card'>
+              <h5>Dependable</h5>
+            </article>
+          </div>
+          
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Chat</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './nav.css'
+import { useState } from 'react'
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Home</a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}>Projects</a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>About</a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Contact</a>
+    </nav>
   )
 }
 
